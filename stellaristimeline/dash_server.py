b43e434b75f727885637edef9f99d88c2e14f398
eliasdoehne <elias.doehne@hemio.de>
2018-02-11 18:12:54 +0100

    dash should work now

--- stellaristimeline/dash_server.py
+++ stellaristimeline/dash_server.py
@@ -17,7 +17,10 @@ app.scripts.config.serve_locally = True
 
 
 def populate_available_games() -> Dict[str, models.Game]:
-    return {g.game_name: g for g in models.Game.query.all()}
+    session = models.SessionFactory()
+    games = {g.game_name: g for g in (session.query(models.Game).all())}
+    session.close()
+    return games
 
 
 AVAILABLE_GAMES = populate_available_games()
@@ -25,24 +28,39 @@ SELECTED_GAME_NAME = None
 if AVAILABLE_GAMES:
     SELECTED_GAME_NAME = next(iter(AVAILABLE_GAMES.keys()))
 
+DEFAULT_PLOT_LAYOUT = go.Layout(yaxis={"type": "linear"})
 app.layout = html.Div([
     html.H1('Select your game'),
-    dcc.Dropdown(
-        id='select-game-dropdown',
-        options=[
-            {'label': g, 'value': g} for g in AVAILABLE_GAMES
-        ],
-        value=SELECTED_GAME_NAME,
-    ),
+    html.Div([
+        dcc.Dropdown(
+            id='select-game-dropdown',
+            options=[
+                {'label': g, 'value': g} for g in AVAILABLE_GAMES
+            ],
+            value=SELECTED_GAME_NAME,
+        ),
+        dcc.Dropdown(
+            id='select-timewindow-dropdown',
+            options=[
+                {
+                    "5 years": 5,
+                    "10 years": 5,
+                    "50 years": 5,
+                    "all": 100000,
+                }
+            ],
+            value="all",
+        ),
+    ]),
     html.H1('Population count'),
     dcc.Graph(
         id='pop-count-graph',
-        figure={"layout": go.Layout(yaxis={"type": "log"})},
+        figure={"layout": DEFAULT_PLOT_LAYOUT},
     ),
     html.H1('Surveyed Systems'),
     dcc.Graph(
         id='survey-count-graph',
-        figure={"layout": go.Layout(yaxis={"type": "log"})},
+        figure={"layout": DEFAULT_PLOT_LAYOUT},
     ),
     dcc.Interval(
         id='interval-component',
@@ -53,9 +71,7 @@ app.layout = html.Div([
 
 
 def get_plot_data() -> visualization.EmpireProgressionPlotData:
-    session = models.SessionFactory()
-    game: models.Game = session.query(models.Game).filter(models.Game.game_name == SELECTED_GAME_NAME).one()
-    return visualization.get_current_execution_plot_data(game)
+    return visualization.get_current_execution_plot_data(SELECTED_GAME_NAME)
 
 
 def get_plot_lines(plot_data: visualization.EmpireProgressionPlotData, plot_data_dict: Dict[str, Any]) -> List[Dict[str, Any]]:
@@ -71,20 +87,32 @@ def get_plot_lines(plot_data: visualization.EmpireProgressionPlotData, plot_data
 
 def update_selected_game(new_selected_game):
     global SELECTED_GAME_NAME
-    assert new_selected_game in AVAILABLE_GAMES
-    SELECTED_GAME_NAME = new_selected_game
+    if new_selected_game != SELECTED_GAME_NAME:
+        assert new_selected_game in AVAILABLE_GAMES
+        print(f"Selected game is {new_selected_game}")
+        SELECTED_GAME_NAME = new_selected_game
 
 
 @app.callback(Output('pop-count-graph', 'figure'), [Input('interval-component', 'n_intervals'), Input('select-game-dropdown', 'value')])
-def update_graph(n, value):
-    print(f"Selected game is {value}")
+def update_pop_graph(n, value):
     update_selected_game(value)
     start = time.time()
     plot_data = get_plot_data()
     plot_list = get_plot_lines(plot_data, plot_data.pop_count)
     end = time.time()
     print(f"Update took {end - start} seconds!")
-    return {'data': plot_list}
+    return {'data': plot_list, 'layout': DEFAULT_PLOT_LAYOUT}
+
+
+@app.callback(Output('survey-count-graph', 'figure'), [Input('interval-component', 'n_intervals'), Input('select-game-dropdown', 'value')])
+def update_survey_graph(n, value):
+    update_selected_game(value)
+    start = time.time()
+    plot_data = get_plot_data()
+    plot_list = get_plot_lines(plot_data, plot_data.survey_count)
+    end = time.time()
+    print(f"Update took {end - start} seconds!")
+    return {'data': plot_list, 'layout': DEFAULT_PLOT_LAYOUT}
 
 
 @app.callback(Output('select-game-dropdown', 'options'), [Input('interval-component', 'n_intervals')])
