5e0bcad74864040c3d5acd114ff37209ca35b3c4
eliasdoehne <elias.doehne@hemio.de>
2018-03-07 12:01:21 +0100

    Add streamhandler to stdout

--- src/stellarisdashboard/main.py
+++ src/stellarisdashboard/main.py
@@ -1,16 +1,24 @@
 import logging
 import pathlib
+import sys
 import threading
-
 import time
 
-from stellardashboard import cli, dash_server, visualization
+from stellarisdashboard import cli, dash_server
 
 BASE_DIR = pathlib.Path.home() / ".local/share/stellaristimeline/"
 STELLARIS_SAVE_DIR = pathlib.Path.home() / ".local/share/Paradox Interactive/Stellaris/save games/"
 logging.basicConfig(level=logging.INFO, filename=BASE_DIR / "stellaris_dashboard.log")
 
 logger = logging.getLogger(__name__)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root_logger.addHandler(ch)
 
 
 def main():
@@ -21,10 +29,12 @@ def main():
     t_dash = threading.Thread(target=dash_server.start_server, daemon=False, args=())
     t_dash.start()
     while True:
+        # update the selected game when a save game from a different game is detected. This is a workaround since the game selection dropdown
+        # from dash does not seem to work in the in-game browser.
         time.sleep(5)
         if cli.MOST_RECENTLY_UPDATED_GAME is not None and cli.MOST_RECENTLY_UPDATED_GAME != dash_server.SELECTED_GAME_NAME:
-            print("Updating selected game in dash!")
-            print(cli.MOST_RECENTLY_UPDATED_GAME)
+            logger.debug("Updating selected game in dash!")
+            logger.debug(cli.MOST_RECENTLY_UPDATED_GAME)
             dash_server.update_selected_game(cli.MOST_RECENTLY_UPDATED_GAME)
 
 
