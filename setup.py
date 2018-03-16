5ad0c82450bbe51f2349a536a05cb3d72be2993c
eliasdoehne <elias.doehne@hemio.de>
2018-03-16 20:06:37 +0100

    Ignore cython extensions on windows (short term solution)

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import platform
+
 from setuptools import setup, find_packages
 
 try:
     from Cython.Build import cythonize
 
     extension_modules = cythonize("src/stellarisdashboard/cython_ext/token_value_stream.pyx")
+    # Currently, the cython extensions do not work on windows. For now, disable them
+    if platform.platform() == "Windows":
+        extension_modules = []
+
 except ImportError:
     print("Cython is not installed, using slow parser.")
     extension_modules = []
 except RuntimeError as e:
-    print(e)
+    print(f"Warning: RuntimeError while preparing Cython extension: {e}")
     print("Falling back to slow parser.")
     extension_modules = []
 
