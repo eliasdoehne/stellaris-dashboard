9ec8575fa3096ff9c8228bc20c8812c748ba46e0
eliasdoehne <elias.doehne@hemio.de>
2018-03-27 02:49:53 +0200

    Fixed quotation escape issue in re-based tokenizer, added some tokenizer unittests

--- src/stellarisdashboard/token_value_stream_re.py
+++ src/stellarisdashboard/token_value_stream_re.py
@@ -4,7 +4,7 @@ WHITESPACE = re.compile('[\t\n ]*')
 EQ = re.compile('=')
 BR_OPEN = re.compile('{')
 BR_CLOSE = re.compile('}')
-Q_STR = re.compile('"[^"]*"')
+Q_STR = re.compile('"((\\\\\")|[^"])*"')
 IDENTIFIER = re.compile('[0-9_]*[a-zA-Z_]+[a-zA-Z0-9_:]*')
 FLOAT = re.compile('-?[0-9]+\.[0-9]*')
 INT = re.compile('-?[0-9]+')
@@ -37,9 +37,6 @@ def token_value_stream(gamestate: str, debug=False):
             match = regex.match(gamestate, pos=start_index)
             if match:
                 match_str = match.group(0)
-                if debug:
-                    yield match_str, line_number
-                else:
-                    yield match_str
+                yield match_str, line_number
                 start_index += len(match_str)
                 break
---
+++ test/token_value_stream_test.py
@@ -0,0 +1,27 @@
+import pytest
+import pyximport
+
+pyximport.install()
+
+from stellarisdashboard import token_value_stream_re
+from stellarisdashboard.cython_ext import token_value_stream
+
+import token_value_stream_test_data
+
+
+@pytest.mark.parametrize("test_data", token_value_stream_test_data.VALID_TOKEN_SEQUENCE_TEST_DATA)
+def test_token_value_stream(test_data):
+    input, expected = test_data
+    tok_re = list(token_value_stream_re.token_value_stream(input))
+    tok_cython = list(token_value_stream.token_value_stream(input))
+
+    tok_re_values_only = [val for val, pos in tok_re]
+    tok_cython_values_only = [val for val, pos in tok_cython]
+    expected_values_only = [val for val, pos in expected]
+    assert tok_re_values_only == expected_values_only
+    assert tok_cython_values_only == expected_values_only
+
+    tok_re_lines = list(token_value_stream_re.token_value_stream(input, debug=True))
+    tok_cython_lines = list(token_value_stream.token_value_stream(input, debug=True))
+    assert tok_re_lines == expected
+    assert tok_cython_lines == expected
