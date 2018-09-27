ce630b4b010e8b04dfc99b7809832198aee67344
eliasdoehne <elias.doehne@hemio.de>
2018-09-27 23:08:59 +0200

    Moved default output path to current directory

--- src/stellarisdashboard/default_paths.py
+++ src/stellarisdashboard/default_paths.py
@@ -1,5 +1,5 @@
 linux_save_path = ".local/share/Paradox Interactive/Stellaris/save games/"
-linux_output_path = "Documents/stellaristimeline/"
+linux_output_path = "output/"
 
 win_save_path = "Documents\Paradox Interactive\Stellaris\save games"
-win_output_path = "Documents/stellaristimeline/"
+win_output_path = "output/"
