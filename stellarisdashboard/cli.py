1c076ad002ca290b31ef77441f5a3bc076d7355b
eliasdoehne <elias.doehne@hemio.de>
2019-02-17 23:06:47 +0100

    Removed old matplotlib code (will be uploaded to another repo soon)

--- stellarisdashboard/cli.py
+++ stellarisdashboard/cli.py
@@ -11,9 +11,8 @@ import multiprocessing as mp
 import threading
 
 import click
-import sqlalchemy
 
-from stellarisdashboard import config, save_parser, timeline, visualization_data, visualization_mpl, models
+from stellarisdashboard import config, save_parser, timeline, visualization_data
 
 logger = logging.getLogger(__name__)
 
@@ -27,81 +26,6 @@ threads_help_string = 'The number of threads that run in parallel when reading s
 @click.group()
 def cli():
     pass
-
-
-@cli.command()
-@click.option('--game-name', default="", type=click.STRING, help=game_name_help_string)
-@click.option('--showeverything', is_flag=True, help=showeverything_help_string)
-def visualize(game_name, showeverything):
-    f_visualize_mpl(game_name, show_everything=showeverything)
-
-
-def f_visualize_mpl(game_name_prefix: str, show_everything=False):
-    """
-    Export a static visualization using matplotlib. Image files are saved to the output folder.
-
-    :param game_name_prefix: Visualizations are generated for all games matching this prefix.
-    :param show_everything: Override the option from the dashboard settings.
-    :return:
-    """
-    config.CONFIG.show_everything = show_everything
-    matching_games = models.get_known_games(game_name_prefix)
-    if not matching_games:
-        logger.warning(f"No game matching {game_name_prefix} was found in the database!")
-    match_games_string = ', '.join(matching_games)
-    logger.info(f"Found matching games {match_games_string} for prefix \"{game_name_prefix}\"")
-    for game_name in matching_games:
-        try:
-            plot_data = visualization_data.EmpireProgressionPlotData(game_name)
-            plot_data.initialize()
-            plot_data.update_with_new_gamestate()
-            plot = visualization_mpl.MatplotLibVisualization(plot_data)
-            plot.make_plots()
-        except sqlalchemy.orm.exc.NoResultFound as e:
-            logger.error(f'No game matching "{game_name}" was found in the database!')
-
-
-@cli.command()
-@click.option('--game-name', default="", type=click.STRING, help=game_name_help_string)
-@click.option('--showeverything', is_flag=True, help=showeverything_help_string)
-def visualize_game_comparison(game_name, showeverything):
-    f_visualize_mpl_comparison(game_name, show_everything=showeverything)
-
-
-def f_visualize_mpl_comparison(game_name_prefix: str, show_everything=True):
-    """
-    Generate a static comparative visualization of multiple games using matplotlib.
-    Useful to AI modders to evaluate how well their changes perform against the default AI.
-
-    Image files are saved to the output folder.
-
-    :param game_name_prefix: Visualizations are generated for all games matching this prefix.
-    :param show_everything: Override the option from the dashboard settings.
-    :return:
-    """
-    config.CONFIG.show_everything = show_everything
-    matching_games = models.get_known_games(game_name_prefix)
-    if not matching_games:
-        logger.warning(f"No game matching {game_name_prefix} was found in the database!")
-        return
-    match_games_string = ', '.join(matching_games)
-    logger.info(f"Found matching games {match_games_string} for prefix \"{game_name_prefix}\"")
-    plot = visualization_mpl.MatplotLibComparativeVisualization(
-        comparison_id=game_name_prefix,
-
-    )
-    for game_name in matching_games:
-        try:
-            plot_data = visualization_data.EmpireProgressionPlotData(game_name)
-            plot_data.initialize()
-            plot_data.update_with_new_gamestate()
-
-            plot.add_data(game_name, plot_data)
-
-        except sqlalchemy.orm.exc.NoResultFound as e:
-            logger.error(f'No game matching "{game_name}" was found in the database!')
-    plot.make_plots()
-
 
 @cli.command()
 @click.option('--save-path', type=click.Path(exists=True, file_okay=False))
