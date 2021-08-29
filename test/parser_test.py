4ac1716811ac3015b97bb0fae620143667835da9
eliasdoehne <blubblubblobb@gmail.com>
2021-08-29 13:36:51 +0200

    formattingh

--- test/parser_test.py
+++ test/parser_test.py
@@ -5,8 +5,7 @@ import parser_test_cases
 
 
 @pytest.mark.parametrize(
-    "test_case",
-    parser_test_cases.PARSER_TEST_CASES,
+    "test_case", parser_test_cases.PARSER_TEST_CASES,
 )
 def test_save_parser_edge_case(test_case):
     data = parser_test_cases.PARSER_TEST_CASES[test_case]
