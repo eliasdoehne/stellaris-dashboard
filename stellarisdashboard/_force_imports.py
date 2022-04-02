46e5f13362cbf4c778275aee88a5d27328d8c02a
Elias Döhne <elias.doehne@gmx.de>
2022-04-02 15:30:12 +0200

    fi

--- stellarisdashboard/_force_imports.py
+++ stellarisdashboard/_force_imports.py
@@ -1,7 +1,9 @@
 def force_scipy_import():
     import numpy as np
     from scipy.spatial import Voronoi
+
     Voronoi(np.array([[0, 0], [0, 1], [1, 0], [1, 1]]))
+
 
 def force_imports():
     force_scipy_import()
