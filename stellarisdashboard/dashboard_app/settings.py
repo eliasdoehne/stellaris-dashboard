cb20e7bec2518125f07b336d1f58896d60ef18ac
Michael Moore <mscottmoore@protonmail.com>
2024-07-07 23:55:29 -0500

    Changes to wording, layout, and style of settings page

--- stellarisdashboard/dashboard_app/settings.py
+++ stellarisdashboard/dashboard_app/settings.py
@@ -20,116 +20,124 @@ def settings_page():
 
     current_values = config.CONFIG.get_adjustable_settings_dict()
     settings = {
-        "check_version": {
-            "type": t_bool,
-            "value": _bool_to_lowercase(current_values["check_version"]),
-            "name": "Check for new versions (only when subscribed in the Steam Workshop)",
-            "description": "Check for new versions (only when subscribed in the Steam Workshop)",
+        "File Locations": {
+            "save_file_path": {
+                "type": t_str,
+                "value": current_values["save_file_path"],
+                "name": "Save files folder *",
+                "description": "Where the dashboard will look for new Stellaris save files. Should contain a folder for each game, and each of those folder should contain one or .sav files. Leave blank to reset to default.",
+            },
+            "stellaris_user_data_path": {
+                "type": t_str,
+                "value": current_values["stellaris_user_data_path"],
+                "name": "Stellaris user data folder *",
+                "description": "Where the dashboard will look for user data such as enabled mods. It should contain the dlc_load.json file. Leave blank to reset to default.",
+            },
+            "stellaris_install_path": {
+                "type": t_str,
+                "value": current_values["stellaris_install_path"],
+                "name": "Stellaris install folder *",
+                "description": "Where the dashboard will look for Stellaris game data. Leave blank to reset to default.",
+            },
         },
-        "save_file_path": {
-            "type": t_str,
-            "value": current_values["save_file_path"],
-            "name": "Save file path (applies after restart, set to empty to restore default, applies after restart)",
-            "description": "Where the dashboard will look for new Stellaris save files.",
+        "Data Visibility": {
+            "show_everything": {
+                "type": t_bool,
+                "value": _bool_to_lowercase(current_values["show_everything"]),
+                "name": "Show information for all countries",
+                "description": "Show information for all countries, including unknown ones.",
+            },
+            "show_all_country_types": {
+                "type": t_bool,
+                "value": _bool_to_lowercase(current_values["show_all_country_types"]),
+                "name": "Show all country types",
+                "description": "Check to include special countries like enclaves, leviathans or the shroud.",
+            },
+            "filter_events_by_type": {
+                "type": t_bool,
+                "value": _bool_to_lowercase(current_values["filter_events_by_type"]),
+                "name": "Filter history ledger by event type",
+                "description": "If enabled, only a selection of relevant historical events are shown in the event ledger, depending on the page.",
+            },
+            "mp_username": {
+                "type": t_str,
+                "value": current_values["mp_username"],
+                "name": "Your multiplayer username",
+                "description": "By default, data for countries controlled by players with any other username are hidden (see below setting). This setting does not affect single-player games.",
+            },
+            "hide_other_players": {
+                "type": t_bool,
+                "value": _bool_to_lowercase(current_values["hide_other_players"]),
+                "name": "Hide other players",
+                "description": "If enabled, hides information for countries controlled by players that do not match the above multiplayer Username",
+            },
         },
-        "stellaris_user_data_path": {
-            "type": t_str,
-            "value": current_values["stellaris_user_data_path"],
-            "name": "Stellaris user data folder",
-            "description": "Where the dashboard will look for user data such as enabled mods. It should contain the dlc_load.json file.",
+        "Performance": {
+            "read_all_countries": {
+                "type": t_bool,
+                "value": _bool_to_lowercase(current_values["read_all_countries"]),
+                "name": "Store data of all countries",
+                "description": "Store budgets and internal stats of all countries. This makes a larger database and could slow things down.",
+            },
+            "save_name_filter": {
+                "type": t_str,
+                "value": current_values["save_name_filter"],
+                "name": "Save file name filter",
+                "description": "Only save files whose file names contain this string are processed. For example, you could use \".01.01\" so only saves from the start of the year are processed. Alternatively, use \"Skip saves after processing\" below",
+            },
+            "skip_saves": {
+                "type": t_int,
+                "value": current_values["skip_saves"],
+                "min": 0,
+                "max": 100,
+                "name": "Skip saves after processing",
+                "description": "After processing a save, the next X saves are skipped. Use this if the dashboard cannot keep up with autosaves. Alternatively, use the \"Save name filter\" above.",
+            },
+            "plot_time_resolution": {
+                "type": t_int,
+                "value": current_values["plot_time_resolution"],
+                "min": 0,
+                "max": 10000,
+                "name": "Initial graph resolution",
+                "description": "Number of points used when first loading the graphs. Set to 0 to load full data.",
+            },
+            "threads": {
+                "type": t_int,
+                "value": current_values["threads"],
+                "min": 1,
+                "name": "Number of threads *",
+                "description": "Number of threads for reading save files.",
+            },
         },
-        "stellaris_install_path": {
-            "type": t_str,
-            "value": current_values["stellaris_install_path"],
-            "name": "Stellaris install folder",
-            "description": "Where the dashboard will look for Stellaris game data.",
-        },
-        "stellaris_language": {
-            "type": t_str,
-            "value": current_values["stellaris_language"],
-            "name": "Stellaris language",
-            "description": "The language to use for localizing Stellaris text. In the format l_<language> (for example, l_english)"
-        },
-        "mp_username": {
-            "type": t_str,
-            "value": current_values["mp_username"],
-            "name": "Your Multiplayer username",
-            "description": "",
-        },
-        "hide_other_players": {
-            "type": t_bool,
-            "value": _bool_to_lowercase(current_values["hide_other_players"]),
-            "name": "Hide other players",
-            "description": "If enabled, hides information for countries controlled by players that do not match the above Multiplayer Username",
-        },
-        "threads": {
-            "type": t_int,
-            "value": current_values["threads"],
-            "min": 1,
-            "name": "Number of threads (applies after restart)",
-            "description": "Number of threads for reading save files, updates apply after restarting the dashboard.",
-        },
-        "show_everything": {
-            "type": t_bool,
-            "value": _bool_to_lowercase(current_values["show_everything"]),
-            "name": "Show information for all countries",
-            "description": "Show information for all countries, including unknown ones.",
-        },
-        "show_all_country_types": {
-            "type": t_bool,
-            "value": _bool_to_lowercase(current_values["show_all_country_types"]),
-            "name": "Show all country types",
-            "description": "Check to include special countries like enclaves, leviathans or the shroud.",
-        },
-        "filter_events_by_type": {
-            "type": t_bool,
-            "value": _bool_to_lowercase(current_values["filter_events_by_type"]),
-            "name": "Filter history ledger by event type",
-            "description": "If enabled, only a selection of relevant historical events are shown in the event ledger, depending on the page.",
-        },
-        "save_name_filter": {
-            "type": t_str,
-            "value": current_values["save_name_filter"],
-            "name": "Save file name filter",
-            "description": "Only save files whose file names contain this string are processed.",
-        },
-        "read_all_countries": {
-            "type": t_bool,
-            "value": _bool_to_lowercase(current_values["read_all_countries"]),
-            "name": "Store data of all countries",
-            "description": "Store budgets and internal stats of all countries. This makes a larger database and could slow things down.",
-        },
-        "skip_saves": {
-            "type": t_int,
-            "value": current_values["skip_saves"],
-            "min": 0,
-            "max": 100,
-            "name": "Skip saves after processing (increase if save processing can't keep up with the game)",
-            "description": "Number of save files skipped for each save file that is processed.",
-        },
-        "plot_time_resolution": {
-            "type": t_int,
-            "value": current_values["plot_time_resolution"],
-            "min": 0,
-            "max": 10000,
-            "name": "Initial graph resolution (set to 0 to load the full data)",
-            "description": "Number of points used when first loading the graphs.",
-        },
-        "plot_width": {
-            "type": t_int,
-            "value": current_values["plot_width"],
-            "min": 300,
-            "max": 2000,
-            "name": "Graph width (pixels)",
-            "description": "Width of graphs.",
-        },
-        "plot_height": {
-            "type": t_int,
-            "value": current_values["plot_height"],
-            "min": 300,
-            "max": 2000,
-            "name": "Graph height (pixels)",
-            "description": "Height of graphs.",
+        "Interface": {
+            "check_version": {
+                "type": t_bool,
+                "value": _bool_to_lowercase(current_values["check_version"]),
+                "name": "Check for new versions",
+                "description": "When using the Workshop mod for in-game access, a message will be displayed if a newer version of the separate dashboard app is available.",
+            },
+            "stellaris_language": {
+                "type": t_str,
+                "value": current_values["stellaris_language"],
+                "name": "Stellaris language *",
+                "description": "The language to use for localizing Stellaris text. In the format l_<language> (for example, l_english)"
+            },
+            "plot_width": {
+                "type": t_int,
+                "value": current_values["plot_width"],
+                "min": 300,
+                "max": 2000,
+                "name": "Graph width",
+                "description": "Width of graphs in pixels.",
+            },
+            "plot_height": {
+                "type": t_int,
+                "value": current_values["plot_height"],
+                "min": 300,
+                "max": 2000,
+                "name": "Graph height",
+                "description": "Height of graphs in pixels.",
+            },
         },
     }
     return render_template("settings_page.html", current_settings=settings)
